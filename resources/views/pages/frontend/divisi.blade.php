65431913a3171eb6e0a7c4dad68ff4a2453779ee
Moh. Syaifudin <msyaifudin640@gmail.com>
2022-05-14 09:48:20 +0700

    Update divisi.blade.php

--- resources/views/pages/frontend/divisi.blade.php
+++ resources/views/pages/frontend/divisi.blade.php
@@ -2,6 +2,85 @@
 
 @section('content')
 
+<section class="probootstrap-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3 text-center section-heading probootstrap-animate">
+              <h2>Divisi PPM Syafi'ur Rohman</h2>
+              <p class="lead">Sed a repudiandae impedit voluptate nam Deleniti dignissimos perspiciatis nostrum porro nesciunt</p>
+            </div>
+          </div>
+      <div class="row">
+        <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate">
+          <a href="#" class="probootstrap-featured-news-box">
+            <figure class="probootstrap-media"><img src="{{ url('frontend/enlight-master/img/img_sm_1.jpg') }}" alt="Free Bootstrap Template by ProBootstrap.com" class="img-responsive"></figure>
+            <div class="probootstrap-text">
+              <h3>Tempora consectetur unde nisi</h3>
+              <span class="probootstrap-date"><i class="icon-calendar"></i>July 9, 2017</span>
+              <span class="probootstrap-location"><i class="icon-location2"></i>White Palace, Brooklyn, NYC</span>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate">
+          <a href="#" class="probootstrap-featured-news-box">
+            <figure class="probootstrap-media"><img src="{{ url('frontend/enlight-master/img/img_sm_2.jpg') }}" alt="Free Bootstrap Template by ProBootstrap.com" class="img-responsive"></figure>
+            <div class="probootstrap-text">
+              <h3>Tempora consectetur unde nisi</h3>
+              <span class="probootstrap-date"><i class="icon-calendar"></i>July 9, 2017</span>
+              <span class="probootstrap-location"><i class="icon-location2"></i>White Palace, Brooklyn, NYC</span>
+            </div>
+          </a>
+        </div>
+        <div class="clearfix visible-sm-block visible-xs-block"></div>
+        <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate">
+          <a href="#" class="probootstrap-featured-news-box">
+            <figure class="probootstrap-media"><img src="{{ url('frontend/enlight-master/img/img_sm_3.jpg') }}" alt="Free Bootstrap Template by ProBootstrap.com" class="img-responsive"></figure>
+            <div class="probootstrap-text">
+              <h3>Tempora consectetur unde nisi</h3>
+              <span class="probootstrap-date"><i class="icon-calendar"></i>July 9, 2017</span>
+              <span class="probootstrap-location"><i class="icon-location2"></i>White Palace, Brooklyn, NYC</span>
+            </div>
+          </a>
+        </div>
 
+        <div class="clearfix visible-md-block"></div>
+
+        <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate">
+          <a href="#" class="probootstrap-featured-news-box">
+            <figure class="probootstrap-media"><img src="{{ url('frontend/enlight-master/img/img_sm_1.jpg') }}" alt="Free Bootstrap Template by ProBootstrap.com" class="img-responsive"></figure>
+            <div class="probootstrap-text">
+              <h3>Tempora consectetur unde nisi</h3>
+              <span class="probootstrap-date"><i class="icon-calendar"></i>July 9, 2017</span>
+              <span class="probootstrap-location"><i class="icon-location2"></i>White Palace, Brooklyn, NYC</span>
+            </div>
+          </a>
+        </div>
+        <div class="clearfix visible-sm-block visible-xs-block"></div>
+
+        <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate">
+          <a href="#" class="probootstrap-featured-news-box">
+            <figure class="probootstrap-media"><img src="{{ url('frontend/enlight-master/img/img_sm_2.jpg') }}" alt="Free Bootstrap Template by ProBootstrap.com" class="img-responsive"></figure>
+            <div class="probootstrap-text">
+              <h3>Tempora consectetur unde nisi</h3>
+              <span class="probootstrap-date"><i class="icon-calendar"></i>July 9, 2017</span>
+              <span class="probootstrap-location"><i class="icon-location2"></i>White Palace, Brooklyn, NYC</span>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 probootstrap-animate">
+          <a href="#" class="probootstrap-featured-news-box">
+            <figure class="probootstrap-media"><img src="{{ url('frontend/enlight-master/img/img_sm_3.jpg') }}" alt="Free Bootstrap Template by ProBootstrap.com" class="img-responsive"></figure>
+            <div class="probootstrap-text">
+              <h3>Tempora consectetur unde nisi</h3>
+              <span class="probootstrap-date"><i class="icon-calendar"></i>July 9, 2017</span>
+              <span class="probootstrap-location"><i class="icon-location2"></i>White Palace, Brooklyn, NYC</span>
+            </div>
+          </a>
+        </div>
+        <div class="clearfix visible-sm-block visible-xs-block"></div>
+
+      </div>
+    </div>
+  </section>
 
 @endsection
