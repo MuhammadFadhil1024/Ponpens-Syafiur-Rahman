2251b818c534f13744e47cb9977b5bde5061c4d4
Moh. Syaifudin <msyaifudin640@gmail.com>
2022-05-16 10:16:52 +0700

    Update detailartikel.blade.php

--- resources/views/pages/frontend/detailartikel.blade.php
+++ resources/views/pages/frontend/detailartikel.blade.php
@@ -10,14 +10,15 @@
   <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,500,500i,600,600i,700,700i,900,900i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{ url('frontend/folio/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ url('frontend/folio/assets2/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
   <link href="{{ url('frontend/folio/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
   <link href="{{ url('frontend/folio/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
   <link href="{{ url('frontend/folio/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
   <link href="{{ url('frontend/folio/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="{{ url('frontend/folio/assets/css/style2.css') }}" rel="stylesheet">
+  <link href="{{ url('frontend/folio/assets/css/style2
+  .css') }}" rel="stylesheet">
 
     <!-- ======= Blog Single ======= -->
     <!--<div class="main-content paddsection">-->
