9b845e4e8160f7f7f4c1e3b3845270f09c5f6918
Moh. Syaifudin <msyaifudin640@gmail.com>
2022-05-29 23:34:03 +0700

    push

--- resources/views/pages/frontend/index.blade.php
+++ resources/views/pages/frontend/index.blade.php
@@ -347,68 +347,7 @@
     </div>
   </section>
 
-  <section class="probootstrap-section">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 col-md-offset-3 text-center section-heading probootstrap-animate">
-          <h2>Mengapa Harus Memilih Pondok Pesantren Mahasiswa Syafi'ur Rohman?</h2>
-          <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-6">
-          <div class="service left-icon probootstrap-animate">
-            <div class="icon"><i class="icon-checkmark"></i></div>
-            <div class="text">
-              <h3>Consectetur Adipisicing</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-            </div>
-          </div>
-          <div class="service left-icon probootstrap-animate">
-            <div class="icon"><i class="icon-checkmark"></i></div>
-            <div class="text">
-              <h3>Aliquid Dolorum Saepe</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-            </div>
-          </div>
-          <div class="service left-icon probootstrap-animate">
-            <div class="icon"><i class="icon-checkmark"></i></div>
-            <div class="text">
-              <h3>Eveniet Tempora Anisi</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="service left-icon probootstrap-animate">
-            <div class="icon"><i class="icon-checkmark"></i></div>
-            <div class="text">
-              <h3>Laboriosam Quod Dignissimos</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-            </div>
-          </div>
 
-          <div class="service left-icon probootstrap-animate">
-            <div class="icon"><i class="icon-checkmark"></i></div>
-            <div class="text">
-              <h3>Asperiores Maxime Modi</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-            </div>
-          </div>
-
-          <div class="service left-icon probootstrap-animate">
-            <div class="icon"><i class="icon-checkmark"></i></div>
-            <div class="text">
-              <h3>Libero Maxime Molestiae</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
-            </div>
-          </div>
-
-        </div>
-      </div>
-      <!-- END row -->
-    </div>
-  </section>
 
 
 @endsection
--- resources/views/pages/frontend/profil.blade.php
+++ resources/views/pages/frontend/profil.blade.php
@@ -24,7 +24,7 @@
               <p><a href="#" class="btn btn-primary">Learn More</a></p>
             </div>
             <div class="probootstrap-image probootstrap-animate" style="background-image: url(img/slider_4.jpg)">
-                <iframe src="https://www.youtube.com/embed/5lGzHr1lhgw" frameborder="0" class="btn-video popup-vimeo"><i class="icon-play3"></i></iframe>
+              <iframe src="https://www.youtube.com/embed/5lGzHr1lhgw" frameborder="0" class="btn-video popup-vimeo"><i class="icon-play3"></i></iframe>
             </div>
           </div>
         </div>
@@ -45,8 +45,70 @@
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis explicabo veniam labore ratione illo vero voluptate a deserunt incidunt odio aliquam commodi blanditiis voluptas error non rerum temporibus optio accusantium!</p>
         <p>Quis explicabo veniam labore ratione illo vero voluptate a deserunt incidunt odio aliquam commodi blanditiis voluptas error non rerum temporibus optio accusantium!</p>
       </div>
+    </div>
+  </section>
 
 
+  <section class="probootstrap-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3 text-center section-heading probootstrap-animate">
+          <h2>VISI & MISI PPM SYAFI'UR ROHMAN?</h2>
+          <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+              <h3>Consectetur Adipisicing</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+            </div>
+          </div>
+          <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+              <h3>Aliquid Dolorum Saepe</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+            </div>
+          </div>
+          <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+              <h3>Eveniet Tempora Anisi</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+              <h3>Laboriosam Quod Dignissimos</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+            </div>
+          </div>
+
+          <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+              <h3>Asperiores Maxime Modi</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+            </div>
+          </div>
+
+          <div class="service left-icon probootstrap-animate">
+            <div class="icon"><i class="icon-checkmark"></i></div>
+            <div class="text">
+              <h3>Libero Maxime Molestiae</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto provident qui tempore natus quos quibusdam soluta at.</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <!-- END row -->
     </div>
   </section>
 
