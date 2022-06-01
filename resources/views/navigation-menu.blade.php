bb737512d21e8304dfe42f19fbaa6c190e40e1cf
Moh. Syaifudin <msyaifudin640@gmail.com>
2022-06-01 16:25:32 +0700

    p

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -37,6 +37,9 @@
                     <x-jet-nav-link href="{{ route('dashboard.registration.index') }}" :active="request()->routeIs('dashboard.registration.index')">
                         {{ __('Registration') }}
                     </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('dashboard.alumni.index') }}" :active="request()->routeIs('dashboard.alumni.index')">
+                        {{ __('Alumni') }}
+                    </x-jet-nav-link>
                     @endif
 
                     @if (Auth::user()->roles == 'SANTRI')
--- resources/views/pages/frontend/profil.blade.php
+++ resources/views/pages/frontend/profil.blade.php
@@ -85,21 +85,23 @@
       <div class="row">
         @foreach ($pengasuh as $pengasuh)
         <div class="col-md-4 col-sm-6">
-          <div class="probootstrap-teacher text-center probootstrap-animate">
-            <figure class="media">
-              <img src="{{ Storage::url($pengasuh->url) }}"alt="Free Bootstrap Template by ProBootstrap.com" class="img-responsive">
-            </figure>
-            <div class="text">
-              <h6>{{$pengasuh->name}}</h6>
-              <h5>{{$pengasuh->job}}</h5>
-              <ul class="probootstrap-footer-social">
-                <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
-                <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
-                <li class="instagram"><a href="#"><i class="icon-instagram2"></i></a></li>
-                <li class="google-plus"><a href="#"><i class="icon-google-plus"></i></a></li>
-              </ul>
+            <div class="item">
+                <div class="probootstrap-teacher text-center probootstrap-animate">
+                    <figure class="media">
+                    <img src="{{ Storage::url($pengasuh->url) }}"alt="Free Bootstrap Template by ProBootstrap.com" class="img-responsive">
+                    </figure>
+                    <div class="text">
+                    <h6>{{$pengasuh->name}}</h6>
+                    <h5>{{$pengasuh->job}}</h5>
+                    <ul class="probootstrap-footer-social">
+                        <li class="twitter"><a href="#"><i class="icon-twitter"></i></a></li>
+                        <li class="facebook"><a href="#"><i class="icon-facebook2"></i></a></li>
+                        <li class="instagram"><a href="#"><i class="icon-instagram2"></i></a></li>
+                        <li class="google-plus"><a href="#"><i class="icon-google-plus"></i></a></li>
+                    </ul>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
         @endforeach
       </div>
